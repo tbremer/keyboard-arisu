9534afa74c4dd747df763af53c901325b15d9876
Tom Bremer <tom@tbremer.com>
2025-02-20 16:43:15 -0600

    add modtap(left-alt, left-gui) to the windows layer

--- keyboards/arisu/keymaps/tbremer/keymap.c
+++ keyboards/arisu/keymaps/tbremer/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,    // qwerty, 15 keys
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,              KC_TRNS,    // asdf, 14 keys
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,                // zxcv, 13 keys
-    KC_TRNS, KC_LGUI, KC_SPC, KC_LALT,                                  KC_TRNS, KC_TRNS,                                        KC_TRNS, KC_TRNS, KC_TRNS      // modifiers, 9 keys
+    KC_TRNS, MT(MOD_LALT, KC_LGUI), KC_SPC, KC_LALT,                    KC_TRNS, KC_TRNS,                                      KC_TRNS, KC_TRNS, KC_TRNS      // modifiers, 9 keys
   ),
 
   // Function layout
